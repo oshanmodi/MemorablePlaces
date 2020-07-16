2a1e1fa0b20ce9c596ab01286ce32d597c242374
oshan modi <modi.oshan@gmail.com>
2020-07-16 17:56:24 +0530

    Locations adding properly on both screens. the PlaceNames list on main activity gets refreshed every time and only one address shows. will try to send entire array of locations in next update.

--- app/src/main/java/com/example/oshan/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/oshan/memorableplaces/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.oshan.memorableplaces;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,30 +18,29 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView placesListView;
     ArrayList<String> placeNames = new ArrayList<>();
+    ArrayList<LatLng> savedLocations = new ArrayList<>();
+    Intent mapsActivityIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 //        add a header to the list view. this add extra space on top which refines the look of the app.
-        placesListView = findViewById(R.id.placesListView);
-        TextView headerTextView;
-        headerTextView = new TextView(this);
-        headerTextView.setText("Your saved places \n");
-        headerTextView.setTextSize(30);
-        headerTextView.setClickable(true);
-        headerTextView.setAllCaps(true);
-        placesListView.addHeaderView(headerTextView);
+        addListViewHeader();
 
 //        add array adapter to the list view. We only have on item at the start.
 //        as user saves more location we keep adding more items
         placeNames.add("add a favorite place");
+
         Context context = getApplicationContext();
         ArrayAdapter placesAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, placeNames);
         placesListView.setAdapter(placesAdapter);
@@ -46,16 +51,45 @@ public class MainActivity extends AppCompatActivity {
                 goToMaps(i);
             }
         });
+
+//        get intent when returning from maps.
+        Intent intent = getIntent();
+        if(intent.getExtras() != null) {
+            Bundle inputFromMap = intent.getExtras();
+            LatLng newLatLng = (LatLng) inputFromMap.get("newLocLatLng");
+            String addressText = (String) inputFromMap.get("newAddress");
+            Log.i("info", newLatLng.toString());
+            if (newLatLng != null) {
+                savedLocations.add(newLatLng);
+                placeNames.add(addressText);
+                Log.i("info", newLatLng.toString());
+            }
+        }
     }
 
     public void goToMaps(int idx){
 //        Log.i("item idx", Integer.toString(idx));
-        Intent mapsActivityIntent = new Intent(getApplicationContext(),MapsActivity.class);
+        mapsActivityIntent = new Intent(getApplicationContext(),MapsActivity.class);
         if(idx == 1){
-            mapsActivityIntent.putExtra("address", "0");
+            mapsActivityIntent.putExtra("address", "Add new location");
         } else {
-            mapsActivityIntent.putExtra("address", placeNames.get(idx));
-        };
+            Log.i("index, lengplaces, lenglocations", Integer.toString(idx) + Integer.toString(placeNames.size()) + Integer.toString(savedLocations.size()));
+            mapsActivityIntent.putExtra("addressText", placeNames.get(idx-1));
+            mapsActivityIntent.putExtra("addressLatLng", savedLocations.get(idx-2));
+        }
         startActivity(mapsActivityIntent);
     }
+
+    public void addListViewHeader(){
+        placesListView = findViewById(R.id.placesListView);
+        TextView headerTextView;
+        headerTextView = new TextView(this);
+        headerTextView.setText("Your saved places \n");
+        headerTextView.setTextSize(30);
+        headerTextView.setClickable(true);
+        headerTextView.setAllCaps(true);
+        placesListView.addHeaderView(headerTextView);
+    }
+
 }
+
--- app/src/main/java/com/example/oshan/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/example/oshan/memorableplaces/MapsActivity.java
@@ -1,8 +1,18 @@
 package com.example.oshan.memorableplaces;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,12 +22,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    LatLng savedLatLng;
+    String savedAddressText;
+    Geocoder geocoder;
     Intent intent;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    Location prevLocation;
 
+    String addressText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +49,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         intent = getIntent();
-        String temp = intent.getStringExtra("address");
-        Toast.makeText(this, temp, Toast.LENGTH_SHORT).show();
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            savedLatLng = (LatLng) intent.getExtras().get("addressLatLng");
+            savedAddressText = (String) intent.getExtras().get("addressText");
+        } catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Add new location", Toast.LENGTH_SHORT).show();
+        }
+
+        //        Get permission to access user location
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+            String provider = LocationManager.GPS_PROVIDER;
+            prevLocation = locationManager.getLastKnownLocation(provider);
+        }
+
     }
 
 
@@ -47,25 +84,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng).title("Your Latitude is " + latLng.toString()));
-            }
-        });
-        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                goToMain();
-            }
-        });
+        if(savedLatLng ==null) {
+            savedLatLng = new LatLng(-34, 151);
+            savedAddressText = "Marker in Sydney";
+        }
+        mMap.addMarker(new MarkerOptions().position(savedLatLng).title(savedAddressText));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(savedLatLng,5));
+        mMap.setOnMapClickListener(onMapClickListener);
+        mMap.setOnMapLongClickListener(onMapLongClickListener);
     }
 
-    public void goToMain(){
-        intent.putExtra("temptext","back from maps");
-        finish();
+    GoogleMap.OnMapClickListener onMapClickListener = new GoogleMap.OnMapClickListener() {
+        @Override
+        public void onMapClick(LatLng latLng) {
+            addMarkerOnMap(latLng);
+        }
+    };
+
+    GoogleMap.OnMapLongClickListener onMapLongClickListener = new GoogleMap.OnMapLongClickListener() {
+        @Override
+        public void onMapLongClick(LatLng latLng) {
+            addMarkerOnMap(latLng);
+            goToMain(latLng);
+        }
+    };
+
+    public void addMarkerOnMap(LatLng latLng){
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Your lat/lng is " + latLng.toString()));
+    }
+
+    public void goToMain(LatLng latLng) {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        intent.putExtra("newLocLatLng", latLng);
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            Log.i("Address", addressList.get(0).toString());
+            addressText = addressList.get(0).getAddressLine(0);
+            Log.i("Address", addressText);
+            intent.putExtra("newAddress", addressText);
+        } catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Location not found", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        startActivity(intent);
     }
 }
